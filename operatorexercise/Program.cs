29d5ef5e82ba9d705f2b762dd23fd62c846c3094
mraeb <mraeburn111@gmail.com>
2020-06-22 13:55:22 -0500

    intial commit

--- operatorexercise/Program.cs
+++ operatorexercise/Program.cs
@@ -5,8 +5,34 @@ namespace operatorexercise
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            
+
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+                        
+            var radius = double.Parse(Console.ReadLine());
+            var answer = AreaOfCircle(radius);
+
+            var userInput = Console.ReadLine();
+                        
+            var area = AreaOfCircle(radius);
+
+            Console.WriteLine($"{area} is this the circle with a radius of (radius)")
+                        
         }
+
+        private static object AreaOfCircle(double radius)
+        {
+           return (Math.PI)*(radius * radius);
+        }
+
+    
     }
 }
